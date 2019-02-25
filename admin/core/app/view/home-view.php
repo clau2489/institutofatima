5471c93a281f577efeef90c91bf4a40c6ed63ba6
OesteDEV <info@oestedev.com>
2019-02-25 16:29:25 -0300

    ...

---
+++ admin/core/app/view/home-view.php
@@ -0,0 +1,12 @@
+    <section class="content-header">
+      <h1>
+        Gestor de contenidos
+        <small>Version 1.0</small>
+      </h1>
+    </section>
+
+<section class="content">
+
+
+
+</section>
